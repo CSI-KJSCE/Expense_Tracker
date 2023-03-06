59f04077b596fc93a27a812eecb7520ad2a35166
aryan <aaryan.p114@gmail.com>
2023-03-06 15:56:13 +0530

    feat: starter code

--- complete_code/script.js
+++ complete_code/script.js
@@ -7,25 +7,10 @@ const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 const notification = document.getElementById("notification");
 
-const dummyTransactions = [
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 },
-];
 
-//let transactions = dummyTransactions;
 
-// LocalStorage is not enabled in CodePen for security reasons
- const localStorageTransactions = JSON.parse(
-   localStorage.getItem("transactions")
-);
-let transactions =
-  localStorageTransactions !== null ? localStorageTransactions : [];
+let transactions = [];
 
-function updateLocaleStorage() {
-  localStorage.setItem("transactions", JSON.stringify(transactions));
-}
 
 function showNotification() {
   notification.classList.add("show");
@@ -51,7 +36,6 @@ function addTransaction(e) {
     transactions.push(transaction);
     addTransactionDOM(transaction);
     updateValues();
-    updateLocaleStorage();
     text.value = "";
     amount.value = "";
   }
@@ -84,18 +68,16 @@ function updateValues() {
       .filter((value) => value < 0)
       .reduce((accumulator, value) => (accumulator += value), 0) * -1
   ).toFixed(2);
-  balance.innerText = `$${total}`;
-  moneyPlus.innerText = `$${income}`;
-  moneyMinus.innerText = `$${expense}`;
+  balance.innerText = `${total}`;
+  moneyPlus.innerText = `${income}`;
+  moneyMinus.innerText = `${expense}`;
 }
 
 function removeTransaction(id) {
   transactions = transactions.filter((transaction) => transaction.id !== id);
-  updateLocaleStorage();
   init();
 }
 
-// Init
 function init() {
   list.innerHTML = "";
   transactions.forEach(addTransactionDOM);
